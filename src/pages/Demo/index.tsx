280e10740f92678e97caaa837b7963914dd7ca65
TKDchampion <as1219123@yahoo.com.tw>
2024-11-05 18:55:08 +0800

    router and bootstrap

---
+++ src/pages/Demo/index.tsx
@@ -0,0 +1,27 @@
+// import AdvanceComponent from '../components/Advance-component';
+// import CalendarCompanent from '../components/Calendar-component';
+// import InputComponent from '../components/Input-component';
+
+function Demo() {
+  return (
+    <div>
+      <h1>Demo</h1>
+      {/* <div className="row">
+        <div className="col-4">
+          <h2>Password Input</h2>
+          <InputComponent isPassword/>
+        </div>
+        <div className="col-4">
+          <h2>Calendar</h2>
+          <CalendarCompanent />
+        </div>
+        <div className="col-4">
+          <h2>Calendar</h2>
+          <AdvanceComponent />
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default Demo;
